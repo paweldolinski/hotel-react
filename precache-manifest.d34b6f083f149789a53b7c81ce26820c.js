ad4380ade876bd6c93210c67bbbf0efbc7c161c0
Pawel <ocebeki@gmail.com>
2020-08-17 10:55:08 +0200

    Updates

---
+++ precache-manifest.d34b6f083f149789a53b7c81ce26820c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5cc2a8aca3d4e5c16157ddaacaa232",
+    "url": "/hotel-react/index.html"
+  },
+  {
+    "revision": "52365081b2e29f6c7d30",
+    "url": "/hotel-react/static/css/main.b7e41f00.chunk.css"
+  },
+  {
+    "revision": "56e89dff29f29edb812d",
+    "url": "/hotel-react/static/js/2.0ce06b21.chunk.js"
+  },
+  {
+    "revision": "52365081b2e29f6c7d30",
+    "url": "/hotel-react/static/js/main.d6db51a8.chunk.js"
+  },
+  {
+    "revision": "adc8665a052cb36728c4",
+    "url": "/hotel-react/static/js/runtime-main.1b6fe825.js"
+  }
+]);
